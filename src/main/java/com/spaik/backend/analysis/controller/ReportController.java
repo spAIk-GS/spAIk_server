7818a3ca9c6df5d491bedcb9e466229cba860e4c
kalli <kedia1004@gmail.com>
2025-08-11 20:27:08 +0900

    Update ReportController.java

--- src/main/java/com/spaik/backend/analysis/controller/ReportController.java
+++ src/main/java/com/spaik/backend/analysis/controller/ReportController.java
@@ -6,21 +6,24 @@ import com.spaik.backend.analysis.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable; 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/report") 
 @RequiredArgsConstructor
 public class ReportController {
 
     private final ReportService reportService;
 
-    @PostMapping
-    public ResponseEntity<ReportResponseDto> createReport(@RequestBody ReportRequestDto requestDto) {
+    @PostMapping("/{presentation_id}")
+    public ResponseEntity<ReportResponseDto> createReport(@PathVariable("presentation_id") Long presentationId) {
         // DTO를 서비스에 전달하여 Report 생성
+        ReportRequestDto requestDto = new ReportRequestDto();
+        requestDto.setPresentationId(presentationId);
+
         ReportResponseDto responseDto = reportService.createReport(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
